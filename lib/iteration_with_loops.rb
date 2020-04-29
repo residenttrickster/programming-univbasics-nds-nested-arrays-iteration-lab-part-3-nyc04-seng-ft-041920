57ada58edec065862021cc06452a1aaca8f8828e
Anastasia Orlova <metkusblackus@gmail.com>
2020-04-29 03:49:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,6 +8,7 @@ def join_nested_strings(src)
     while element_index < src[row_index].count do
       if src[row_index][element_index] = ""
         nested_string = nested_string + src[row_index][element_index]
+      end
       element_index += 1
     end
     row_index += 1
